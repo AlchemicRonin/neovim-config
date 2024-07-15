ac8a6bda7facfa91c0a6aabe16ee1094bc7ddaee
Guang Yin <guangy2@illinois.edu>
2024-07-15 15:09:14 -0500

    add neovide copy paste support

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -3,6 +3,21 @@ vim.cmd([[
   set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff600-blinkon600-Cursor0/lCursor0,sm:block-blinkwait175-blinkoff150-blinkon175
 ]])
 
+if vim.g.neovide then
+    vim.keymap.set("n", "<D-s>", ":w<CR>")      -- Save
+    vim.keymap.set("v", "<D-c>", '"+y')         -- Copy
+    vim.keymap.set("n", "<D-v>", '"+P')         -- Paste normal mode
+    vim.keymap.set("v", "<D-v>", '"+P')         -- Paste visual mode
+    vim.keymap.set("c", "<D-v>", "<C-R>+")      -- Paste command mode
+    vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+
 if not vim.g.neovide then
     return {} -- do nothing if not in a Neovide session
 end
