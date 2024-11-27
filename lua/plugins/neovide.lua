fdaed37f17c245d3e145bb5ed9f866276442a1bd
Guang Yin <guangy2@illinois.edu>
2024-11-27 01:03:52 -0600

    fix(neovide): neovide through server dooesn't load configs for neovide itself

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -18,9 +18,9 @@ vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 
-if not vim.g.neovide then
-    return {} -- do nothing if not in a Neovide session
-end
+-- if not vim.g.neovide then
+--     return {} -- do nothing if not in a Neovide session
+-- end
 
 return {
     "AstroNvim/astrocore",
