6010f221129856d7e1bb4791a6e478b9b4ea8cb3
Guang Yin <guangy2@illinois.edu>
2024-11-30 11:31:20 -0600

    feat(rainbow-delimiters): reorder colors for bracket pairs

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,14 @@
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  opts = {
+    highlight = {
+      "RainbowDelimiterCyan",
+      "RainbowDelimiterViolet",
+      "RainbowDelimiterOrange",
+      "RainbowDelimiterGreen",
+      "RainbowDelimiterRed",
+      "RainbowDelimiterBlue",
+      "RainbowDelimiterYellow",
+    },
+  },
+}
